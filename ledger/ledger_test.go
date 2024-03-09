510ec3de5a8306916e43fa7bb00cc6285aba6f1a
Luis Felipe Hernandez <luis.hernandez093@gmail.com>
2024-03-09 11:38:30 -0500

    Add test for adding transactions to ledger

--- ledger/ledger_test.go
+++ ledger/ledger_test.go
@@ -1,6 +1,8 @@
 package ledger
 
-import "testing"
+import (
+	"testing"
+)
 
 func Test_validateTransaction(t *testing.T) {
 	sendToSelfPerson := &Person{Name: "Jane Smith", Balance: 40}
@@ -46,6 +48,45 @@ func Test_validateTransaction(t *testing.T) {
 			result := validateTransaction(tc.sender, tc.receiver, tc.amount)
 			if (result != nil) != tc.shouldThrow {
 				t.Errorf("%v - incorrect results, expected %v, got %v", tc.name, tc.shouldThrow, result)
+			}
+		})
+	}
+}
+
+func Test_addTransaction(t *testing.T) {
+	ledger := NewLedger()
+
+	john := ledger.Participants[0]
+	jane := ledger.Participants[1]
+
+	testCases := []struct {
+		name         string
+		sender       *Person
+		receiver     *Person
+		amount       int
+		expectedSize int
+	}{
+		{
+			name:         "Valid Transaction",
+			sender:       john,
+			receiver:     jane,
+			amount:       10,
+			expectedSize: len(ledger.Transactions) + 1,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			ledger.addTransaction(tc.sender, tc.receiver, tc.amount)
+
+			if len(ledger.Transactions) != tc.expectedSize {
+				t.Errorf("Expected %d transactions, found %d", tc.expectedSize, len(ledger.Transactions))
+				return
+			}
+
+			lastTransaction := ledger.Transactions[len(ledger.Transactions)-1]
+			if lastTransaction.Sender != tc.sender || lastTransaction.Receiver != tc.receiver || lastTransaction.Amount != tc.amount {
+				t.Errorf("Transaction was not added correctly")
 			}
 		})
 	}
