c11867edb340353729691085a36a0dc86c8b570b
Luis Felipe Hernandez <17690241+lf-hernandez@users.noreply.github.com>
2024-03-11 22:40:20 -0400

    Extract Merkle Tree simulation into function behind merkletree package

--- main.go
+++ main.go
@@ -1,11 +1,9 @@
 package main
 
 import (
-	"crypto/sha256"
 	"fmt"
 
 	"github.com/lf-hernandez/bitcoinplayground/ledger"
-
 	"github.com/lf-hernandez/bitcoinplayground/merkletree"
 )
 
@@ -14,38 +12,5 @@ func main() {
 	ledger.RunSimulation()
 	fmt.Println("\n=========================================================")
 	fmt.Print("\nMerkle Tree simulation\n\n")
-
-	txs := []string{
-		"Felipe sent Satoshi 1 coin",
-		"Jane sent John 5 coins",
-		"Satoshi sent Jane 3 coins",
-		"John sent Felipe 7 coins",
-	}
-	for _, tx := range txs {
-		h := sha256.New()
-		h.Write([]byte(tx))
-		fmt.Printf("tx: %v\nhash: %x\n", tx, h.Sum(nil))
-	}
-	fmt.Println("\nBuilding Merkle tree")
-	root := merkletree.BuildTree(txs, false)
-	tree := &merkletree.MerkleTree{Root: root}
-	tree.PrintTree()
-
-	fmt.Println("\nGenerating Merkle proof")
-	proof := tree.GenerateMerkleProof(txs[3])
-	for _, e := range proof {
-		fmt.Printf("%x\n", e.Hash)
-	}
-
-	h := sha256.New()
-	h.Write([]byte(txs[3]))
-	targetHash := h.Sum(nil)
-
-	fmt.Printf(
-		"\nRunning verification\nis transaction: %v\nwith hash: %x \nin current tree: %v\n",
-		txs[3],
-		targetHash,
-		merkletree.VerifyMerkleProof(targetHash, proof, tree.Root.Hash),
-	)
-
+	merkletree.RunSimulation()
 }
--- merkletree/merkletree.go
+++ merkletree/merkletree.go
@@ -183,3 +183,38 @@ func printNode(node *MerkleNode, level int) {
 		printNode(node.R, level+1)
 	}
 }
+
+func RunSimulation() {
+	txs := []string{
+		"Felipe sent Satoshi 1 coin",
+		"Jane sent John 5 coins",
+		"Satoshi sent Jane 3 coins",
+		"John sent Felipe 7 coins",
+	}
+	for _, tx := range txs {
+		h := sha256.New()
+		h.Write([]byte(tx))
+		fmt.Printf("tx: %v\nhash: %x\n", tx, h.Sum(nil))
+	}
+	fmt.Println("\nBuilding Merkle tree")
+	root := BuildTree(txs, false)
+	tree := &MerkleTree{Root: root}
+	tree.PrintTree()
+
+	fmt.Println("\nGenerating Merkle proof")
+	proof := tree.GenerateMerkleProof(txs[3])
+	for _, e := range proof {
+		fmt.Printf("%x\n", e.Hash)
+	}
+
+	h := sha256.New()
+	h.Write([]byte(txs[3]))
+	targetHash := h.Sum(nil)
+
+	fmt.Printf(
+		"\nRunning verification\nis transaction: %v\nwith hash: %x \nin current tree: %v\n",
+		txs[3],
+		targetHash,
+		VerifyMerkleProof(targetHash, proof, tree.Root.Hash),
+	)
+}
